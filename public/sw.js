6056d163eca13ce4f2095eeb7dae4a71835e12f9
Thilina Ruwan <thilina20250527@gmail.com>
2025-06-22 13:17:01 +0000

    still its not showing install as app?

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,62 @@
-// A very basic service worker to make the app installable.
+const CACHE_NAME = 'student-support-hub-v2';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/favicon.ico',
+];
+
+// Install a service worker
 self.addEventListener('install', (event) => {
   console.log('Service Worker: Installing...');
-  // An empty install handler is sufficient to make the app installable.
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Service Worker: Caching app shell');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+  // We only want to cache GET requests.
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  
+  event.respondWith(
+    // Try the network first
+    fetch(event.request)
+      .then((res) => {
+        // If the request was successful, cache it and return it
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => {
+        // If the network fails, try to get it from the cache
+        return caches.match(event.request).then((res) => {
+            return res;
+        })
+      })
+  );
+});
+
+// Update a service worker
 self.addEventListener('activate', (event) => {
   console.log('Service Worker: Activating...');
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  // A minimal fetch handler is required for the app to be installable.
-  // This basic handler simply passes the request through to the network.
-  event.respondWith(fetch(event.request));
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Service Worker: Deleting old cache', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
