552469cf1afbecdcf247293f4801800e85608f5a
Thilina Ruwan <thilina20250527@gmail.com>
2026-01-11 06:37:53 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: DialogTrigger is

--- src/app/admin/manage/games/medimind/levels/[id]/page.tsx
+++ src/app/admin/manage/games/medimind/levels/[id]/page.tsx
@@ -5,7 +5,7 @@ import { useState, useMemo, useEffect } from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose, DialogTrigger } from '@/components/ui/dialog';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import { Input } from '@/components/ui/input';
 import { toast } from '@/hooks/use-toast';
